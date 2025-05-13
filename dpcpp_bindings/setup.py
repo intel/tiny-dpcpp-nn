cab746caaef325e0132b8898c0ae0342a704e768
Benjamin Ummenhofer <benjamin.ummenhofer@intel.com>
2025-05-13 09:59:52 +0000

    build system fixes

--- dpcpp_bindings/setup.py
+++ dpcpp_bindings/setup.py
@@ -1,158 +1,87 @@
 import os
-import re
-import subprocess
-import sys
-from pathlib import Path
-import torch
+from setuptools import setup
 
-from setuptools import Extension, setup
-from setuptools.command.build_ext import build_ext
+from intel_extension_for_pytorch.xpu.cpp_extension import (
+    DPCPPExtension,
+    DpcppBuildExtension,
+    IS_LINUX,
+)
 
-# Convert distutils Windows platform specifiers to CMake -A arguments
-PLAT_TO_CMAKE = {
-    "win32": "Win32",
-    "win-amd64": "x64",
-    "win-arm32": "ARM",
-    "win-arm64": "ARM64",
-}
+# Here ze_loader is not necessary, just used to check libraries linker
+# libraries = ["ze_loader"] if IS_LINUX else []
+libraries = []
 
+dpcpp_path = os.getenv("CMPLR_ROOT")
+dpcpp_sycl_path = os.path.join(dpcpp_path, "include", "sycl")
 
-# A CMakeExtension needs a sourcedir instead of a file list.
-# The name must be the _single_ output extension from the CMake build.
-# If you need multiple extensions, see scikit-build.
-class CMakeExtension(Extension):
-    def __init__(self, name: str, sourcedir: str = "") -> None:
-        super().__init__(name, sources=[])
-        self.sourcedir = os.fspath(Path(sourcedir).resolve())
+conda_path = os.getenv("CONDA_PREFIX")
+conda_sycl_path = os.path.join(conda_path, "include", "sycl")
 
-
-class CMakeBuild(build_ext):
-    def build_extension(self, ext: CMakeExtension) -> None:
-        # Must be in this form due to bug in .resolve() only fixed in Python 3.10+
-        ext_fullpath = Path.cwd() / self.get_ext_fullpath(ext.name)
-        extdir = ext_fullpath.parent.resolve()
-
-        # Using this requires trailing slash for auto-detection & inclusion of
-        # auxiliary "native" libs
-
-        debug = int(os.environ.get("DEBUG", 0)) if self.debug is None else self.debug
-        cfg = "Debug" if debug else "Release"
-
-        # CMake lets you override the generator - we need to check this.
-        # Can be set with Conda-Build, for example.
-        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
-
-        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
-        # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
-        # from Python.
-        target_device = os.environ.get("TARGET_DEVICE", "ARC")
-
-        # Validate TARGET_DEVICE
-        if target_device not in {"ARC", "PVC"}:
-            raise ValueError(
-                "TARGET_DEVICE environment variable must be either 'ARC', 'PVC'."
-            )
-
-        cmake_args = [
-            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
-            f"-DPYTHON_EXECUTABLE={sys.executable}",
-            f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
-            f"-DTORCH_CMAKE_PREFIX_PATH={torch.utils.cmake_prefix_path}",
-            "-DCMAKE_CXX_FLAGS=-fPIC",
-            f"-DTARGET_DEVICE={target_device}",
-            "-D BUILD_TEST=OFF",
-            "-D BUILD_BENCHMARK=OFF",
-            "-D BUILD_PYBIND=ON",
-        ]
-        build_args = []
-        # Adding CMake arguments set as environment variable
-        # (needed e.g. to build for ARM OSx on conda-forge)
-        if "CMAKE_ARGS" in os.environ:
-            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
-
-        # In this example, we pass in the version to C++. You might not need to.
-        cmake_args += [f"-DEXAMPLE_VERSION_INFO={self.distribution.get_version()}"]
-
-        if self.compiler.compiler_type != "msvc":
-            # Using Ninja-build since it a) is available as a wheel and b)
-            # multithreads automatically. MSVC would require all variables be
-            # exported for Ninja to pick it up, which is a little tricky to do.
-            # Users can override the generator with CMAKE_GENERATOR in CMake
-            # 3.15+.
-            if not cmake_generator or cmake_generator == "Ninja":
-                try:
-                    import ninja
-
-                    ninja_executable_path = Path(ninja.BIN_DIR) / "ninja"
-                    cmake_args += [
-                        "-GNinja",
-                        f"-DCMAKE_MAKE_PROGRAM:FILEPATH={ninja_executable_path}",
-                    ]
-                except ImportError:
-                    pass
-
-        else:
-            # Single config generators are handled "normally"
-            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
-
-            # CMake allows an arch-in-generator style for backward compatibility
-            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
-
-            # Specify the arch if using MSVC generator, but only if it doesn't
-            # contain a backward-compatibility arch spec already in the
-            # generator name.
-            if not single_config and not contains_arch:
-                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
-
-            # Multi-config generators have a different way to specify configs
-            if not single_config:
-                cmake_args += [
-                    f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{cfg.upper()}={extdir}"
-                ]
-                build_args += ["--config", cfg]
-
-        if sys.platform.startswith("darwin"):
-            # Cross-compile support for macOS - respect ARCHFLAGS if set
-            archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
-            if archs:
-                cmake_args += ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
-
-        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
-        # across all generators.
-        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
-            # self.parallel is a Python 3 only way to set parallel jobs by hand
-            # using -j in the build_ext call, not supported by pip or PyPA-build.
-            if hasattr(self, "parallel") and self.parallel:
-                # CMake 3.12+ only.
-                build_args += [f"-j{self.parallel}"]
-
-        build_temp = Path(self.build_temp) / ext.name
-        if not build_temp.exists():
-            build_temp.mkdir(parents=True)
-
-        subprocess.run(
-            ["cmake", ext.sourcedir, *cmake_args],
-            cwd=build_temp,
-            check=True,
-            capture_output=False,
-        )
-
-        subprocess.run(
-            ["cmake", "--build", ".", *build_args],
-            cwd=build_temp,
-            check=True,
-            capture_output=False,
-        )
-
-
-# The information here can also be placed in setup.cfg - better separation of
-# logic and declaration, and simpler if you include description/version in a file.
 setup(
-    name="tiny_dpcpp_nn_pybind_module",
+    name="tiny_dpcpp_nn",
     version="0.0.1",
-    description="A test project using pybind11 and CMake",
-    long_description="",
-    ext_modules=[CMakeExtension("tiny_dpcpp_nn_pybind_module", "../")],
-    cmdclass={"build_ext": CMakeBuild},
-    python_requires=">=3.7",
+    description="Python bindings for the tiny-dpcpp-nn library",
+    packages=["tiny_dpcpp_nn"],
+    ext_modules=[
+        DPCPPExtension(
+            "tiny_dpcpp_nn.tiny_dpcpp_nn_pybind_module",
+            [
+                "tiny_dpcpp_nn/pybind_module.cpp",
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdfp1664none.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "SwiftNetMLP.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdfp1664sigmoid.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdfp1616none.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdfp1632sigmoid.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdbf16128none.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdfp16128relu.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdfp16128none.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "SwiftNetMLPbf1664.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdbf1632relu.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimd.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdbf1616none.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "SwiftNetMLPfp1664.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdfp1616relu.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdfp16128sigmoid.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "SwiftNetMLPfp1632.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdbf16128relu.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdfp1664relu.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "SwiftNetMLPbf1632.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "SwiftNetMLPfp16128.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "SwiftNetMLPfp1616.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdbf1664relu.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdbf1664sigmoid.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdbf1632sigmoid.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "SwiftNetMLPbf1616.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdbf1664none.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdbf1616relu.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdfp1616sigmoid.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdbf1616sigmoid.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdfp1632none.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "SwiftNetMLPbf16128.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdfp1632relu.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdbf1632none.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "network", "kernel_esimdbf16128sigmoid.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "optimizers", "sgd.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "optimizers", "adam.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "common", "SyclGraph.cpp"),
+                os.path.join(os.path.dirname(__file__), "..", "source", "common", "common.cpp"),
+            ],
+            libraries=libraries,
+            extra_compile_args={'cxx': ['-DTARGET_DEVICE=1', '-std=c++20', '-fPIC']},
+            include_dirs=(
+                [dpcpp_sycl_path]
+                if not os.path.exists(conda_sycl_path)
+                else [conda_sycl_path]
+                + [
+                    os.path.join(os.path.dirname(__file__), "../include"),
+                    os.path.join(os.path.dirname(__file__), "../include/network"),
+                    os.path.join(os.path.dirname(__file__), "../include/common"),
+                    os.path.join(os.path.dirname(__file__), "../include/encodings"),
+                    os.path.join(os.path.dirname(__file__), "../include/optimizers"),
+                    os.path.join(os.path.dirname(__file__), "../extern/json"),
+                ]
+            ),
+        )
+    ],
+    cmdclass={"build_ext": DpcppBuildExtension.with_options(use_ninja=True)},
 )
